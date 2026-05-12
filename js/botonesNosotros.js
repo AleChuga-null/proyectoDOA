fc3358a23394772642445ecf1e93555ac4ec4836
Aleksandra Voinescu <ale17voi@gmail.com>
2026-05-12 10:26:57 +0200

    Integrado el funcionamiento de cambio de bloques dependiendo del botón pulsado en sección Nosotros.

---
+++ js/botonesNosotros.js
@@ -0,0 +1,34 @@
+
+
+// función de cambio
+function mostrarContenido(seccion) {
+    // buscar y ocultar bloques
+    const bloques = document.querySelectorAll('.contenido-flex');
+    bloques.forEach(bloque => {
+        bloque.style.display = 'none';
+    });
+
+    // mostrar solo el que se ha pulsado
+    document.getElementById('bloque-' + seccion).style.display = 'flex';
+
+    // quitar 'active' y ponerlo al pulsado
+    document.querySelectorAll('.tab-btn').forEach(btn => {
+        btn.classList.remove('active');
+    });
+
+    // cambio del título h2
+    const h2 = document.getElementById('encabezado-principal');
+
+    switch (seccion) {
+    case'mision':
+        h2.innerText = "¿Quiénes Somos?";
+        break;
+
+    case 'vision':
+        h2.innerText = "Hacia dónde vamos";
+        break;
+
+    case 'valores':
+        h2.innerText = "";
+    }
+}
